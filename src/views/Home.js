3c46f5693f9186eaf23d99d77ec4ad49bd6d7ded
Isakiye Afasha <afashaisakiye@gmail.com>
2021-03-14 09:17:57 +0300

    commented unsed

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
 // import sections
-// import Hero from '../components/sections/Hero';
-// import FeaturesTiles from '../components/sections/FeaturesTiles';
-// // import FeaturesSplit from '../components/sections/FeaturesSplit';
-// import Testimonial from '../components/sections/Testimonial';
+import Hero from '../components/sections/Hero';
+import FeaturesTiles from '../components/sections/FeaturesTiles';
+// import FeaturesSplit from '../components/sections/FeaturesSplit';
+import Testimonial from '../components/sections/Testimonial';
 // import Cta from '../components/sections/Cta';
 
 const Home = () => {
 
   return (
-    <div>
-      {/* <Hero className="illustration-section-01" />
+    <>
+      <Hero className="illustration-section-01" />
       <FeaturesTiles />
-      <Testimonial topDivider /> */}
+      <Testimonial topDivider />
       {/* <Cta split /> */}
-      Hello World
-    </div>
+    </>
   );
 }
 
