0652738ed2bb1fcb7b7712f1881ae754c1c19d00
Afasha Isakiye <afasha.isakiye@tmcg.co.ug>
2021-10-01 16:28:58 +0300

    Add a 404 simple page

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import LayoutDefault from './layouts/LayoutDefault';
 // Views 
 import Home from './views/Home';
 import Projects from './views/Projects';
+import NotFound from './views/NotFound';
 
 // Initialize Google Analytics
 ReactGA.initialize(process.env.REACT_APP_GA_CODE);
@@ -39,6 +40,7 @@ const App = () => {
         <Switch>
           <AppRoute exact path="/" component={Home} layout={LayoutDefault} />
           <AppRoute exact path="/projects" component={Projects} layout={LayoutDefault} />
+          <AppRoute exact path="*" component={NotFound} layout={LayoutDefault} />
         </Switch>
       )} />
   );
